f3bb1fadde77d8e1d405dae6c27d7a2b4dbec607
IceAccount <isaaclucasgamer3@gmail.com>
2025-09-16 14:05:29 -0300

    Create começo_do_sofrimento.py

---
+++ codein/começo_do_sofrimento.py
@@ -0,0 +1,20 @@
+# @cikey fd9a243ad4a8b28900905b2856e18403
+# @sid 20251174010022
+# @aid 0
+
+
+#begin_inputs
+num1 = 2
+num2 = 2 
+# Defina as variáveis de entrada aqui
+# Não apague as linhas com #begin_inputs e #end_inputs
+
+#end_inputs
+
+# Faça o processamento e a exibição da saída a partir daqui
+	
+
+Soma = num1 + num2
+
+print("Aqui sua soma!")
+print(Soma)
