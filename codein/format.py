d4eaba66d164b1d26e0fce69c8feba0d85e339db
IceAccount <isaaclucasgamer3@gmail.com>
2025-09-16 14:28:24 -0300

    Create format.py

---
+++ codein/format.py
@@ -0,0 +1,17 @@
+# @cikey fd9a243ad4a8b28900905b2856e18403
+# @sid 20251174010022
+# @aid V2.1
+
+
+#begin_inputs
+x = 4
+a = 6
+b = 2
+y = 8
+# defina as variáveis aqui
+
+
+#end_inputs
+
+
+#os prints vão a partir daqui
