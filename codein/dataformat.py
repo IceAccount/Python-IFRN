83a05ade1c027fb7ccf9da407df2fe09e7bdde4c
IceAccount <isaaclucasgamer3@gmail.com>
2025-09-17 17:20:18 -0300

    coisa novinha para o github

---
+++ codein/dataformat.py
@@ -0,0 +1,13 @@
+# @cikey fd9a243ad4a8b28900905b2856e18403
+# @sid 20251174010022
+# @aid V2.2
+
+
+#begin_inputs
+dia = 3
+mes = 7
+ano = 2011
+
+#end_inputs
+
+print(f"{dia}/{mes}/{ano}")
---
+++ codein/media.py
@@ -0,0 +1,12 @@
+# @cikey fd9a243ad4a8b28900905b2856e18403
+# @sid 20251174010022
+# @aid V2.3
+
+
+#begin_inputs
+a = 0
+b = 0
+c = 0
+
+#end_inputs
+
