87a36b0ce7339a5547da4fe6bd307e585a78a51e
IceAccount <isaaclucasgamer3@gmail.com>
2025-09-23 14:28:42 -0300

    oi

---
+++ codein/bisexto.py
@@ -0,0 +1,12 @@
+# @cikey fd9a243ad4a8b28900905b2856e18403
+# @sid 20251174010022
+# @aid V3.3
+
+
+#begin_inputs
+
+#end_inputs
+i = 2004
+for i in range(2004):
+    print(i)
+    i += 4
---
+++ codein/suicidio2.py
@@ -0,0 +1,14 @@
+# @cikey fd9a243ad4a8b28900905b2856e18403
+# @sid 20251174010022
+# @aid V3.2
+
+
+#begin_inputs
+
+# Isso está aqui apenas para grantir que os testes automáticos funcionem
+# NÃO mexa aqui.
+
+#end_inputs
+
+for i in range(99, 250):
+    print(i , "bugs no software,pegue um deles e conserte...\nTecle “Ctrl+F5”\nVamos fazer mais um café!")
