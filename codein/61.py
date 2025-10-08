b774bab4203c8c4242aebd809f7d6f2b8e590559
IceAccount <isaaclucasgamer3@gmail.com>
2025-10-08 17:48:58 -0300

    oioi

---
+++ codein/61.py
@@ -0,0 +1,11 @@
+# @cikey fd9a243ad4a8b28900905b2856e18403
+# @sid 20251174010022
+# @aid V6.1
+
+
+#begin_inputs
+
+#end_inputs
+import random
+for n in range(6):
+    print(random.randint(1,51))
---
+++ codein/62.py
@@ -0,0 +1,8 @@
+# @cikey fd9a243ad4a8b28900905b2856e18403
+# @sid 20251174010022
+# @aid V6.2
+
+
+#begin_inputs
+
+#end_inputs
