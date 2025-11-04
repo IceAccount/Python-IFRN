bacd2bbaf28cfc2f5f4d1743c1e3e1309606f37b
IceAccount <isaaclucasgamer3@gmail.com>
2025-11-04 13:32:32 -0300

    Update 72.py

--- codein/72.py
+++ codein/72.py
@@ -4,6 +4,45 @@
 
 
 #begin_inputs
+palavra = "revenant"
+letras = []
+#end_inputs 
+chances = 4
 
-#end_inputs
+ganhou = False
 
+while True:
+
+    for letra in palavra:
+
+        if letra.lower() in letras:
+            print(letra, end=" ")
+        else:
+            print("_", end=" ")
+
+    print(f"Você tem {chances} chances")
+    tentativa = input("Escolha uma letra para adivinhar: ")
+
+    letras.append(tentativa.lower())
+
+    if tentativa.lower() not in palavra.lower():
+
+        chances -= 1
+
+    ganhou = True
+
+    for letra in palavra:
+
+        if letra.lower() not in letras:
+
+            ganhou = False
+
+    if chances == 0 or ganhou:
+
+        break
+
+if ganhou:
+    print(f"ganhou, the palavra era {palavra}")
+else:
+    print(f"perdeukkkkkkkkkk era {palavra}")
+    
