65d3e60ae5ea7e056e15a2e306079a43e19e5bb9
IceAccount <isaaclucasgamer3@gmail.com>
2025-12-09 14:23:58 -0300

    fgf

---
+++ estudos/manipulador.py
@@ -0,0 +1,17 @@
+import json
+
+caminho_arquivo = 'estudos/carros.json'
+
+def ler_carros():
+    f = open(caminho_arquivo, "r")
+    return json.load(f)
+
+
+
+def salvar_carro(lista):
+    f = open(caminho_carro)
+
+
+
+def adicionar_carro(carro):
+    salvar_carro(carros)
---
+++ estudos/mteste.py
@@ -0,0 +1,10 @@
+import manipulador as ma
+
+carro = {
+    'chass1': "329842932",
+    "marca": "Honda",
+    "modelo": "civic",
+    "ano": 2020
+}
+
+ma.ler_carros()
