fb45f18365bfd494e63778664f4905966efba31c
IceStul <isaaclucasgamer3@gmail.com>
2026-05-06 13:14:56 -0300

    Coleções completed

---
+++ POO/Coleções/1.py
@@ -0,0 +1,5 @@
+import random
+
+Loteria = random.sample(range(1, 41), 25)
+Loteria.sort()
+print(f"seu número {Loteria}")
---
+++ POO/Coleções/2.py
@@ -0,0 +1,19 @@
+contagem = {}
+
+while True:
+    entrada = input("Ano de nascimento: ")
+    
+    if entrada == "":
+        break
+    ano = int(entrada)
+    
+    if ano in contagem:
+        contagem[ano] = contagem[ano] + 1
+    else:
+        contagem[ano] = 1
+
+print("Relatório:")
+
+for ano in contagem:
+    quantidade = contagem[ano]
+    print("Ano", ano, ":", quantidade, "pessoa(s)")
