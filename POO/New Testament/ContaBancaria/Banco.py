cad164806689bece203d0eec4c2510c34101f2d8
IceStul <isaaclucasgamer3@gmail.com>
2026-06-30 15:46:21 -0300

    poo

--- POO/New Testament/ContaBancaria/Banco.py
+++ POO/New Testament/ContaBancaria/Banco.py
@@ -1,102 +1,6 @@
 import tkinter as tk
 from tkinter import messagebox, simpledialog
-
-class Endereço:
-    def __init__(self, rua, numero, bairro, cidade):
-        self.__rua = rua
-        self.__numero = int(numero)
-        self.__bairro = bairro
-        self.__cidade = cidade
-    
-    def get_rua(self):
-        return self.__rua
-    
-    def get_numero(self):
-        return self.__numero
-
-    def get_bairro(self):
-        return self.__bairro
-    
-    def get_cidade(self):
-        return self.__cidade
-
-    def exibir_dados(self):
-        return f'Rua: {self.__rua}, numero: {self.__numero}; \nBairro: {self.__bairro} \nCidade {self.__cidade}'
-
-
-class Cliente:
-    def __init__(self, nome, cpf, endereço):
-        self.__nome = nome
-        self.__cpf = cpf
-        self.__endereço = endereço
-        self.__contas = []
-    
-    def get_nome(self):
-        return self.__nome
-    
-    def get_cpf(self):
-        return self.__cpf
-    
-    def get_endereco(self):
-        return self.__endereço
-    
-    def adicionar_conta(self, cnt):
-        self.__contas.append(cnt)
-    
-    def exibir_dados(self):
-        return f'Nome: {self.__nome} \nCPF: {self.__cpf} \nEndereço: {self.__endereço}'
-
-class ContaBancaria:
-    numeros_contas = []
-    def __init__(self, t, n, s):
-        self.__titular = t
-        self.__numero = n
-        self.__saldo = s
-
-        ContaBancaria.numeros_contas.append(n)
-    def get_titular(self):
-        return self.__titular.get_nome()
-    
-    def get_numero(self):
-        return self.__numero
-    
-    def get_saldo(self):
-        return self.__saldo
-    
-    def exibir_dados(self):
-        return f'CONTA:\nTitular: {self.__titular.get_nome()}\nNumero da conta: {self.__numero}\nSaldo: R$ {self.__saldo}\nCpf: {self.__titular.get_cpf()} \n\nENDEREÇO:\n{self.__titular.get_endereco().exibir_dados()}
-    
-    def depositar(self, value):
-        if value <= 0:
-            return False
-        self.__saldo += value                                            
-        return True
-
-    def sacar(self, value):
-        if value <= 0:
-            return False
-        self.__saldo -= value
-        return True
-    def transferir(self, valor, conta_destino):
-        if valor > 0:
-            self.__saldo -= valor
-            conta_destino.depositar(valor)
-            return True
-        else:
-            return False       
-    @classmethod
-    def contas_duplicadas(cls):
-        repetidos = set()
-        n_repetidos = []
-        for x in cls.numeros_contas:
-            if ContaBancaria.numeros_contas.count(x) > 1:
-                repetidos.add(x)
-            else:
-                n_repetidos.append(x)
-        return f'{repetidos}'
-    @classmethod
-    def verificar_conta_duplicada(cls):
-        return len(cls.numeros_contas) != len(set(cls.numeros_contas))
+from Classes import Cliente, ContaBancaria, Endereço
 
 class BancoApp:
     def __init__(self, janela):
@@ -116,7 +20,7 @@ class BancoApp:
             ContaBancaria(cliente4, 1004, 300),
         ]
 
-        if(self.contas[0].existe_conta_duplicada()):
+        if(self.contas[0].verificar_conta_duplicada()):
             messagebox.showerror("Erro","Existe conta duplicada")
             messagebox.showinfo("contas",self.contas[0].contasduplicadas())
             exit()
---
+++ POO/New Testament/ContaBancaria/Classes.py
@@ -0,0 +1,102 @@
+class Endereço:
+    def __init__(self, rua, numero, bairro, cidade):
+        self.__rua = rua
+        self.__numero = int(numero)
+        self.__bairro = bairro
+        self.__cidade = cidade
+    
+    def get_rua(self):
+        return self.__rua
+    
+    def get_numero(self):
+        return self.__numero
+
+    def get_bairro(self):
+        return self.__bairro
+    
+    def get_cidade(self):
+        return self.__cidade
+
+    def exibir_dados(self):
+        return f'Rua: {self.__rua}, numero: {self.__numero}; \nBairro: {self.__bairro} \nCidade {self.__cidade}'
+
+
+class Cliente:
+    def __init__(self, nome, cpf, endereço):
+        self.__nome = nome
+        self.__cpf = cpf
+        self.__endereço = endereço
+        self.__contas = []
+    
+    def get_nome(self):
+        return self.__nome
+    
+    def get_cpf(self):
+        return self.__cpf
+    
+    def get_endereco(self):
+        return self.__endereço
+    
+    def adicionar_conta(self, cnt):
+        self.__contas.append(cnt)
+    
+    def exibir_dados(self):
+        return f'Nome: {self.__nome} \nCPF: {self.__cpf} \nEndereço: {self.__endereço}'
+
+class ContaBancaria:
+    numeros_contas = []
+    def __init__(self, titular, numero, saldo):
+        self.__titular = titular
+        self.__numero = numero
+        self.__saldo = saldo
+
+        ContaBancaria.numeros_contas.append()
+    def get_titular(self):
+        return self.__titular.get_nome()
+    
+    def get_numero(self):
+        return self.__numero
+    
+    def get_saldo(self):
+        return self.__saldo
+    
+    def exibir_dados(self):
+        return f"CONTA:\nTitular: {self.__titular.get_nome()}\nNumero da conta: {self.__numero}\nSaldo: R$ {self.__saldo}\nCpf: {self.__titular.get_cpf()} \n\nENDEREÇO:\n{self.__titular.get_endereco().exibir_dados()}"
+    
+    def depositar(self, value):
+        if value <= 0:
+            return False
+        self.__saldo += value                                            
+        return True
+
+    def sacar(self, value):
+        if value <= 0:
+            return False
+        self.__saldo -= value
+        return True
+    def transferir(self, valor, conta_destino):
+        if valor > 0:
+            self.__saldo -= valor
+            conta_destino.depositar(valor)
+            return True
+        else:
+            return False       
+    @classmethod
+    def contas_duplicadas(cls):
+        repetidos = set()
+        n_repetidos = []
+        for x in cls.numeros_contas:
+            if ContaBancaria.numeros_contas.count(x) > 1:
+                repetidos.add(x)
+            else:
+                n_repetidos.append(x)
+        return f'{repetidos}'
+    @classmethod
+    def verificar_conta_duplicada(cls):
+        return len(cls.numeros_contas) != len(set(cls.numeros_contas))
+    
+class ContaCorrente(ContaBancaria):
+    def __init__(self, titular, numero, saldo, limite, tarifa_mensal):
+        super().__init__(titular, numero, saldo)
+        self.__limite = limite
+        self.__tarifa_mental = tarifa_mensal
