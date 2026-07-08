8d0ccb8d4aa4d6e8fb5b4c8c237d84437bb1493a
IceStul <isaaclucasgamer3@gmail.com>
2026-07-08 13:39:53 -0300

    .

--- estudos/e.py
+++
@@ -1,6 +0,0 @@
-x = 10
-
-print(f"{x} uau")
-
-c = int(input("coloque numero"))
-print(c+x)
--- estudos/empresas.py
+++
@@ -1,8 +0,0 @@
-nomearchive = "../Python-IFRN/estudos/Phasmophobia"
-f = open(nomearchive)
-
-for t in f.readlines():
-    print(t, end="")
-
-#f.write("Escrever no arquivo")
-#f.write("mais uma linha")
