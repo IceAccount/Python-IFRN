9676bfff96465d9c828b7b612c7b04ca0b2b5b91
IceStul <isaaclucasgamer3@gmail.com>
2026-07-14 14:31:14 -0300

    .
    ee

--- POO/New Testament/ContaBancaria/Banco.py
+++ POO/New Testament/ContaBancaria/Banco.py
@@ -10,13 +10,14 @@ class BancoApp:
 
         endereço1 = Endereço("Enfermaria", 3, "Esquerda superior", "Skeld")
         endereço2 = Endereço("Laboratório", 4, "Direita superior", "Polus outpost")
+        endereço3 = Endereço("дорога","436", "Красноярск", "элита")
         
         cliente1  = Cliente("Ciano", "004.045", endereço1)
         cliente2 = Cliente("Lima", "023.450", endereço2)        
 
         self.contas = [
             ContaCorrente(cliente1, 1001, 500, 1000, 100),
-            ContaPoupanca(cliente2, 1002, 1000, 0.1)
+            ContaPoupanca(cliente2, 1002, 1000, 0.1),
         ]
 
         # messagebox.showinfo("Sucesso", "Depósito realizado.")
